f4669bd55f298088505b9ba391f283403cef0959
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-11-03 17:15:01 -0200

    Incluindo recurso target para poder recuperar o Json

--- src/main/java/br/jus/tse/corporativa/util/camel/CamelSpringBootApplication.java
+++ src/main/java/br/jus/tse/corporativa/util/camel/CamelSpringBootApplication.java
@@ -2,14 +2,23 @@ package br.jus.tse.corporativa.util.camel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-@ImportResource({"classpath:META-INF/spring/camel-context.xml"})
-public class CamelSpringBootApplication {
+@Configuration
+@ImportResource({ "classpath:META-INF/spring/camel-context.xml" })
+public class CamelSpringBootApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CamelSpringBootApplication.class, args);
 	}
-	
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/target/**").addResourceLocations("/target/", "file:target/");
+	}
+
 }
